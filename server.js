5cf0c44cc7182ce4987828922eec5d43acd31a79
russellpipal <russell.pipal@gmail.com>
2016-04-06 11:35:04 -0500

    shows today's date

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(request, response){
-  response.send("Hello World!");
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd='0'+dd
+  }
+
+  if(mm<10) {
+      mm='0'+mm
+  }
+
+  today = mm+'/'+dd+'/'+yyyy;
+
+  response.send("Today is " + today);
 });
 
 var server = app.listen(process.env.PORT || 3000, function(){
